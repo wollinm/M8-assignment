38f06f2f284cfc57948be42a539913dfe5178dae
Magdalena Wollin <mwollino@hotmail.com>
2023-11-15 09:19:03 -0800

    first commit

---
+++ js/script-array-practice.js
@@ -0,0 +1,33 @@
+//STEP 1
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
